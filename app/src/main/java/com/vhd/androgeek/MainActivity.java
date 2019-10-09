a40cc1ad967ac7ccba44d0e29b37f8bdb90f2b3c
althafvly <althafvly@gmail.com>
2019-10-09 12:35:54 +0530

    first commit

---
+++ app/src/main/java/com/vhd/androgeek/MainActivity.java
@@ -0,0 +1,28 @@
+package com.vhd.androgeek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int splash_time = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        }, splash_time);
+    }
+}
