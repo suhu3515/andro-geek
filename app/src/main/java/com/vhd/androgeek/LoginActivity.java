2298bc1d0a7e47213d2cee0e20d17b517ab306cd
suhu3515 <suhu3515@gmail.com>
2019-10-15 23:40:54 +0530

    Login Java done, Verify xml and java file pending. - DAY2

--- app/src/main/java/com/vhd/androgeek/LoginActivity.java
+++ app/src/main/java/com/vhd/androgeek/LoginActivity.java
@@ -2,13 +2,40 @@ package com.vhd.androgeek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText et_mobile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        et_mobile = findViewById(R.id.et_mob);
+
+        Button btn_mob = findViewById(R.id.btn_mob);
+        btn_mob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String mobile = et_mobile.getText().toString().trim();
+                if(mobile.isEmpty() || mobile.length() < 10)
+                {
+                    et_mobile.setError("Enter a valid mobile...");
+                    et_mobile.requestFocus();
+                    return;
+                }
+
+                Intent otp_intent = new Intent(LoginActivity.this,verifyActivity.class);
+                otp_intent.putExtra("mobile",mobile);
+                startActivity(otp_intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/vhd/androgeek/verifyActivity.java
@@ -0,0 +1,14 @@
+package com.vhd.androgeek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class verifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify);
+    }
+}
