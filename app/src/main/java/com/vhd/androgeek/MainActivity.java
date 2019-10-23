cea6925683b4e674e761eb547e19c1a88044bf1b
suhu3515 <suhu3515@gmail.com>
2019-10-23 21:12:33 +0530

    Login completed... (DAY 4)

--- app/src/main/java/com/vhd/androgeek/MainActivity.java
+++ app/src/main/java/com/vhd/androgeek/MainActivity.java
@@ -6,23 +6,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static int splash_time = 3000;
+
+    private FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(loginIntent);
-                finish();
-            }
-        }, splash_time);
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(mUser != null)
+        {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent homeIntent = new Intent(MainActivity.this,homeActivity.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+            }, splash_time);
+        }
+        else
+        {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
+                    startActivity(loginIntent);
+                    finish();
+                }
+            }, splash_time);
+        }
     }
 }
